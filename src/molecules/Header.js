c91cd33f52405acaab8e317ec2e40743e58aad95
Gerardo Valenzuela <ilovesciencefb@gmail.com>
2018-02-18 00:43:39 -0800

    changes to header

--- src/molecules/Header.js
+++ src/molecules/Header.js
@@ -12,33 +12,35 @@ const Header = ({ content, className, images }) => (
     <div className={ `row ${ className }` }>
         { content.filter(item => item.type === 'header').map((item) => [
             <div className='col-sm-12 center' key={ `title-${ item.type }` }>
-                <Title shade='white' size='3x'>{ item.title }</Title>
+                <Title shade='white' size='3.5x' size-md='6x'>{ item.title }</Title>
             </div>,
             <div className='col-sm-12 center' key={ `body-${ item.type }` }>
                 <Text shade='#FADA5E' size='2x'>{ item.body } { '\n' } <Typing hideCursorDelay={ 500 } defaultElement='' startTypingDelay={ 400 } cycle cycleType='erase' children={ content.find(item => item.type === 'cycle').snippets }/></Text>
             </div>,
-            <div className='col-sm-12 center' key={ `button-${ item.type }` }>
-                <Button icon='terminal' type='standard' bold fill='#FADA5E' shade='white' stroke='none' onClick={ () => scrollIntoView(document.getElementById('contact')) }>Hire Me</Button>
-            </div>,
-            <div className='col-sm-12 center learn-more' key={ `learn-more-${ item.type }` }>
-                <Button icon='hand-o-down' type='graphic' bold fill='none' stroke='#FADA5E' shade='#FADA5E' stretch onClick={ () => scrollIntoView(document.getElementById('skills')) }>Learn more about me</Button>
-            </div> ])
+            <div className='col-sm-12 center buttons' key={ `row-${ item.type }`}>
+                <div className='row'>
+                    <div className='col-sm-12 col-lg-2 col-lg-offset-4 center' key={ `button-${ item.type }` }>
+                        <Button icon='terminal' type='standard' bold fill='#FADA5E' shade='white' stroke='none' onClick={ () => scrollIntoView(document.getElementById('contact')) }>Hire Me</Button>
+                    </div>
+                    <div className='col-sm-12 col-lg-2 center learn-more' key={ `learn-more-${ item.type }` }>
+                        <Button icon='hand-o-down' type='standard' bold fill='none' stroke='#FADA5E' shade='#FADA5E' stretch onClick={ () => scrollIntoView(document.getElementById('skills')) }>Learn more</Button>
+                    </div>
+                </div>
+            </div>
+             ])
         }
     </div>
 );
 
 export default styled(Header)`
-    padding-bottom: 5em;
+    padding-bottom: 8.5em;
     >div>h1 {
-        padding-bottom: 3em;
-    }
-    &:last-child > .learn-more {
-        padding-top: 12em;
+        padding-bottom: 1.9em;
     }
     >div>p {
         height: 3em;
         padding-top: 3em;
-        padding-bottom: 2em;
+        padding-bottom: 4em;
     }
     >div>p>span {
         font-size: 1em!important;
@@ -51,13 +53,15 @@ export default styled(Header)`
         }
     ` }
     ${ media.medium`
-        padding-top: 2em;
+    padding-bottom: 13em;
         >div>h1 {
-            font-size: 4em;
+            padding-bottom: 1.3em;
         }
+        padding-top: 2em;
         >div>p {
             font-size: 2em;
             height: 2em;
+            padding-bottom: 6em;
         }
     ` }
 `;
--- src/organisms/Introduction.js
+++ src/organisms/Introduction.js
@@ -23,18 +23,22 @@ const mapStateToProps = ({ content, images }) => ({ content, images });
 export default connect(mapStateToProps)(styled(Introduction)`
     padding: 0;
     margin-top: 0;
+    >div {
+        -webkit-background-size: 100% auto!important;
+        -moz-background-size: 100% auto!important;
+        -o-background-size: 100% auto!important;
+        background-size: 100% auto!important;
+    }
     ${ media.small`
         >div {
-            background: #474B56 no-repeat center center fixed;
-            -webkit-background-size: cover;
-            -moz-background-size: cover;
-            -o-background-size: cover;
-            background-size: cover;
+            background: linear-gradient(90deg, #000000, #434343);
+            /*background: linear-gradient(90deg, #2C3E50, #FD746C);
+            background: #474B56 no-repeat center center fixed;*/
         }
     ` };
-    ${ media.medium`
+    ${ media.large`
         >div {
-            background: ${ props => props.images ? `url(${ props.images.filter(picture => picture.name === 'header')[0].location })` : 'grey' } no-repeat center center fixed;            
+            background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), ${ props => props.images ? `url(${ props.images.filter(picture => picture.name === 'header')[0].location })` : 'grey' };
         }
     ` };
 `);
